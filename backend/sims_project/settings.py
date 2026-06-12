1e6eb6b3cd28cca6f3f8c7154fc31a20fd38b8cc
judejos <judejos2302@gmail.com>
2026-06-12 23:37:20 +0530

    roles chnage user and passoword

--- backend/sims_project/settings.py
+++ backend/sims_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
