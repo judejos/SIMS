4c6a4ce0c754ecba17d8b1dbbb83ae9c7244a6c3
judejos <judejos2302@gmail.com>
2026-06-14 22:30:37 +0530

    login fix

--- backend/sims_project/settings.py
+++ backend/sims_project/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-1234567890')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
